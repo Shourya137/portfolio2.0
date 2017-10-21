ccca10c3714a90bde83f376facf1afec7eb332ec
linuk <hello.linuk@gmail.com>
2017-10-21 21:46:33 +0100

    Updated logo, favicon and fix project card read more button bug

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Navbar from 'components/navbar/Navbar';
-import Navlogo from './JLLogoCircle.svg';
 import Footer from './Footer';
 
 const Layout = ({ children, data: { brand, navItemsStart, navItemsEnd } }) => (
   <div className="layout">
     <Navbar
-      brand={{ ...brand, url: Navlogo }}
+      brand={{ ...brand }}
       navbarEnd={navItemsEnd}
       navItemsStart={navItemsStart}
     />
