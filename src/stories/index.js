ec1b89074c6fe98ff1c4b2526f62e8551557ce35
linuk <hello.linuk@gmail.com>
2018-04-29 16:56:11 +0100

    Updated Fasion Kanvas Project

---
+++ src/stories/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Button, Welcome } from '@storybook/react/demo';
+import StartArrow from '../components/startArrow/StartArrow';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('MyComponent', module).add('Start arror', () => <StartArrow />);
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
